d38355eeb4ed8f6ccd2a1c51b0fe7aef7d64d63d
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-04 23:44:36 +0530

    variables file for Terraform cloud

--- variables.tf
+++ variables.tf
@@ -7,13 +7,11 @@ variable "region" {
 variable "access_key" {
         description = "access_key"
         type        = string
-        default     = "AKIATTSKFVUKOQNPJ77D"
 }
 
 variable "secret_key" {
         description = "Secret access key"
         type        = string
-        default     = "nfJJKyCNYJBRmMNSm6LGjRZNqRL8Q1GOMrHxzyys"
 
 }
 
@@ -29,10 +27,179 @@ variable "key_pair" {
   default     = "abc"
 }
 
-variable "ami_id" {
-  description = "ami id for workspace instances"
+variable "vpc_cidr" {
+  description = "cidr block for VPC"
   type        = string
-  default     = "ami-047126e50991d067b"
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+        description = "Name of VPC"
+        type        = string
+        default     = "Cloud-VPC"
+}
+
+variable "subnet_cidr_public_1" {
+	description = "CIDR block for public subnet 1"
+	type	    = string
+	default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_1_name" {
+  type        = string
+  description = "Name for the public subnet 1"
+  default     = "Public-Cloud1"
+}
+
+variable "availability_zone_1" {
+  type        = string
+  description = "The Availability Zone for the public subnet 1"
+  default     = "ap-southeast-1a" 
 }
 
 
+variable "subnet_cidr_public_2" {
+	description = "CIDR block for public subnet 2"
+	type	    = string
+	default     = "10.0.2.0/24"
+}
+
+variable "public_subnet_2_name" {
+  type        = string
+  description = "Name for the public subnet 2"
+  default     = "Public-Cloud2"
+}
+
+variable "availability_zone_2" {
+  type        = string
+  description = "The Availability Zone for the public subnet 2"
+  default     = "ap-southeast-1b" 
+}
+
+variable "subnet_cidr_private_1" {
+	description = "CIDR block for private subnet 1"
+	type	    = string
+	default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_1_name" {
+  type        = string
+  description = "Name for the private subnet 1"
+  default     = "Private-Cloud1"
+}
+
+variable "subnet_cidr_private_2" {
+	description = "CIDR block for private subnet 2"
+	type	    = string
+	default     = "10.0.4.0/24"
+}
+
+variable "private_subnet_2_name" {
+  type        = string
+  description = "Name for the private subnet 2"
+  default     = "Private-Cloud2"
+}
+
+
+variable "availability_zone_3" {
+  type        = string
+  description = "The Availability Zone for the private subnet 2"
+  default     = "ap-southeast-1c" 
+}
+
+variable "internet_gateway_name" {
+  type = string
+  description = "Name of the Internet Gateway"
+  default = "cloud-igw"
+}
+
+variable "allow_all_ip" {
+  type = string
+  description = "For allowing all i addresses"
+  default = "0.0.0.0/0"
+}
+
+variable "public_route_table_name" {
+  type        = string
+  description = "Name for the public route table"
+  default     = "cloud-public-RT"
+}
+
+variable "nat_gateway_name" {
+  type = string
+  description = "Name of the NAT Gateway"
+  default = "cloud-NAT"
+}
+
+ariable "private_route_table_name" {
+  type        = string
+  description = "Name for the private route table"
+  default     = "cloud-private-RT"
+}
+
+
+variable "frontend_security_group_name" {
+  type        = string
+  description = "Name for the Frontend security group"
+  default     = "FrontendSecurityGroup"
+}
+
+variable "ssh_port" {
+  type        = number
+  description = "SSH port number"
+  default     = 22
+}
+
+variable "tcp_protocol" {
+  type = string
+  description = "Tcp protocol"
+  default = "tcp"
+}
+
+variable "frontend_ami_id" {
+  description = "AMI ID for the frontend instance"
+  type        = string
+  default     = "ami-04c401fa961fbfdda" 
+}
+
+variable "frontend_name" {
+  description = "Name for the frontend instance"
+  type        = string
+  default     = "FrontendInstance"
+}
+
+variable "backend_security_group_name" {
+  type        = string
+  description = "Name for the Backend security group"
+  default     = "BackendSecurityGroup"
+}
+
+variable "backend_ami_id" {
+  description = "AMI ID for the backend instance"
+  type        = string
+  default     = "ami-0b79bb0ef09eb0fe2"  
+}
+
+variable "backend_name" {
+  description = "Name for the Backend instance"
+  type        = string
+  default     = "BackendInstance"
+}
+
+variable "database_security_group_name" {
+  type        = string
+  description = "Name for the Database security group"
+  default     = "DatabaseSG"
+}
+
+variable "database_ami_id" {
+  description = "AMI ID for the database instance"
+  type        = string
+   default     = "ami-0fc98e06ffb6551f5"
+}
+
+variable "database_name" {
+  description = "Name for the Database instance"
+  type        = string
+  default     = "DatabaseInstance"
+}
